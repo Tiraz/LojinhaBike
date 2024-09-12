3ad341abd6ccf04ffea29abe739105f12754963a
Tiraz <hugo.suterio@hotmail.com>
2024-09-12 20:21:10 -0300

    botando os botão pra funcionar

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -1,6 +1,7 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LoginActivity extends AppCompatActivity {
+    MaterialButton btnRecuperarSenhaLogin,btnCadastrarLogin,btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,17 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnRecuperarSenhaLogin = findViewById(R.id.btnRcuperarSenhaLogin);
+        btnCadastrarLogin = findViewById(R.id.btnCadastrarLogin);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+
+        btnCadastrarLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
