ffe379297e4abd99d2ed88148b23c362276b1a30
Tiraz <hugo.suterio@hotmail.com>
2024-09-19 21:26:00 -0300

    menuzin funcional

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -1,15 +1,18 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
-
+    CardView menuBike, menuManutencao, menuAcessorio, menuDica, menuLocais, menuEventos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,36 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        menuBike = findViewById(R.id.menuBike);
+        menuAcessorio = findViewById(R.id.menuAcessorio);
+        menuManutencao = findViewById(R.id.menuManutencao);
+        menuDica = findViewById(R.id.menuDicas);
+        menuLocais = findViewById(R.id.menuLocais);
+        menuEventos = findViewById(R.id.menuEventos);
+
+        menuBike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),BikeActivity.class));
+                finish();
+            }
+        });
+        menuAcessorio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AcessoriosActivity.class));
+                finish();
+            }
+        });
+        menuManutencao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ManutencaoActivity.class));
+                finish();
+            }
+        });
+        menuDica.setOnClickListener();
+
+
     }
 }
