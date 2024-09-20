8ea11b6b4b49d05df0764a5248f539e581439b6b
Tiraz <hugo.suterio@hotmail.com>
2024-09-19 22:10:19 -0300

    menuzin fufando

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -11,8 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuPrincipalActivity extends AppCompatActivity {
+public class MenuPrincipalActivity extends AppCompatActivity implements View.OnClickListener {
+
     CardView menuBike, menuManutencao, menuAcessorio, menuDica, menuLocais, menuEventos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,7 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         menuBike = findViewById(R.id.menuBike);
         menuAcessorio = findViewById(R.id.menuAcessorio);
         menuManutencao = findViewById(R.id.menuManutencao);
@@ -30,29 +33,40 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         menuLocais = findViewById(R.id.menuLocais);
         menuEventos = findViewById(R.id.menuEventos);
 
-        menuBike.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),BikeActivity.class));
-                finish();
-            }
-        });
-        menuAcessorio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),AcessoriosActivity.class));
-                finish();
-            }
-        });
-        menuManutencao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),ManutencaoActivity.class));
-                finish();
-            }
-        });
-        menuDica.setOnClickListener();
 
+        menuBike.setOnClickListener(this);
+        menuAcessorio.setOnClickListener(this);
+        menuManutencao.setOnClickListener(this);
+        menuDica.setOnClickListener(this);
+        menuLocais.setOnClickListener(this);
+        menuEventos.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.menuBike) {
+            startActivity(new Intent(getApplicationContext(), BikeActivity.class));
+            finish();
+        }
+        if (v.getId() == R.id.menuAcessorio) {
+            startActivity(new Intent(getApplicationContext(), AcessoriosActivity.class));
+            finish();
+        }
+        if (v.getId() == R.id.menuManutencao) {
+            startActivity(new Intent(getApplicationContext(), ManutencaoActivity.class));
+            finish();
+        }
+        if (v.getId() == R.id.menuDicas) {
+            startActivity(new Intent(getApplicationContext(), DicasActivity.class));
+            finish();
+        }
+        if (v.getId() == R.id.menuLocais) {
+            startActivity(new Intent(getApplicationContext(), LocaisActivity.class));
+            finish();
+        }
+        if (v.getId() == R.id.menuEventos) {
+            startActivity(new Intent(getApplicationContext(), LocaisActivity.class));
+            finish();
+        }
     }
 }
